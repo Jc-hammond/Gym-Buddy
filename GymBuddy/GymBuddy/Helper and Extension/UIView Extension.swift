5f60f9ab5f8b42966e987efb898102d0a2a350ed
James Chun <jameswchun@gmail.com>
2021-06-23 03:24:08 -0400

    AddGoalVC updated

--- GymBuddy/GymBuddy/Helper and Extension/UIView Extension.swift
+++ GymBuddy/GymBuddy/Helper and Extension/UIView Extension.swift
@@ -20,5 +20,40 @@ extension UIView {
         self.backgroundColor = color
     }
     
+}//End of extension
+
+extension UIView {
+    //JCHUN - Always use an anchor func for programatic constraints!
+    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, trailing: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeading: CGFloat, paddingTrailing: CGFloat, width: CGFloat? = nil, height: CGFloat? = nil) {
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let leading = leading {
+            self.leadingAnchor.constraint(equalTo: leading, constant: paddingLeading).isActive = true
+        }
+        
+        if let trailing = trailing {
+            self.trailingAnchor.constraint(equalTo: trailing, constant: -paddingTrailing).isActive = true //constant must be negative for trailing
+            
+        }
+        
+        if let width = width {
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        
+    }//end of func
     
 }//End of extension
+
