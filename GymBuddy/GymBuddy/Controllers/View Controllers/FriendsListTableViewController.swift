ffb2bd20ad117360a12c4f564057976e5193a3a6
James Chun <jameswchun@gmail.com>
2021-07-02 14:05:56 -0400

    working on adding friends

--- GymBuddy/GymBuddy/Controllers/View Controllers/FriendsListTableViewController.swift
+++ GymBuddy/GymBuddy/Controllers/View Controllers/FriendsListTableViewController.swift
@@ -12,22 +12,25 @@ class FriendsListTableViewController: UITableViewController {
     //MARK: - Properties
     //var buttonTitles: [String]?
     var allUsers: [User]?
-
+    var friendRequests: [FriendRequest]?
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationController?.navigationBar.tintColor = .customLightGreen
         
+        fetchFriendRequests()
         fetchAllUsers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        fetchFriendRequests()
         fetchAllUsers()
     }
-
+    
     //MARK: - Functions
     func fetchAllUsers() {
         let predicate = NSPredicate(value: true)
@@ -47,38 +50,57 @@ class FriendsListTableViewController: UITableViewController {
         }
     }
     
+    func fetchFriendRequests() {
+        guard let currentUser = UserController.shared.currentUser,
+              let friendRequestRefs = currentUser.friendRequestRefs else { return }
+        
+        FriendRequestController.shared.fetchFriendRequests(friendRequestRefs: friendRequestRefs) { result in
+            switch result {
+            case .success(let friendRequests):
+                print("successfully fetched friendRequests")
+                self.friendRequests = friendRequests
+            case .failure(let error):
+                print("Error in \(#function) : On Line \(#line) : \(error.localizedDescription) \n---\n \(error)")
+            }
+        }
+    }
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         guard let allUsers = allUsers else { return 0 }
         return allUsers.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath) as? FriendTableViewCell else { return UITableViewCell() }
         
         guard let allUsers = allUsers,
               let currentUser = UserController.shared.currentUser else { return UITableViewCell() }
-                
-        for user in allUsers {
-            if currentUser.friends.contains(user) {
-                cell.buttonTitle = "added"
-            } else if let currentUserFriendRequests = currentUser.friendRequests {
-                for friendRequest in currentUserFriendRequests {
-                    if friendRequest.accepted == false && friendRequest.ownerDidSend == true {
-                        cell.buttonTitle = "pending"
-                    } else if friendRequest.accepted == false && friendRequest.ownerDidSend == false {
-                        cell.buttonTitle = "accept"
-                    }
-                }
-            } else {
-                cell.buttonTitle = "add friend"
-            }
-        }
         
         let user = allUsers[indexPath.row]
+        let userRef = CKRecord.Reference(recordID: user.recordID, action: .none)
+        let friendRequestForUser = friendRequests?.filter{ $0.friendUserRef == userRef }.first
         
+        if let friendRefs = currentUser.friendRefs,
+           friendRefs.contains(userRef) {
+            
+            cell.buttonTitle = "added"
+            
+        } else if friendRequestForUser != nil {
+            
+            if friendRequestForUser!.ownerDidSend == true {
+                cell.buttonTitle = "pending"
+            } else {
+                cell.buttonTitle = "accept"
+            }
+            
+        } else {
+            cell.buttonTitle = "add friend"
+        }
+        
+        cell.friendRequest = friendRequestForUser
         cell.user = user
         
         return cell
@@ -88,15 +110,79 @@ class FriendsListTableViewController: UITableViewController {
         return view.frame.height/16
     }
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
 }//End of class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        for user in allUsers {
+//            if currentUser.friends.contains(user) {
+//                cell.buttonTitle = "added"
+//            }
+//
+//            let userRef = CKRecord.Reference(recordID: user.recordID, action: .none)
+//            if currentUser.friendRequestRefs?.contains(userRef) {
+//
+//            }
+//
+
+//            else if let currentUserFriendRequests = currentUser.friendRequests {
+//
+//
+//
+//                for friendRequest in currentUserFriendRequests {
+//
+//                    if friendRequest.friendUserRef.recordID == user.recordID && friendRequest.accepted == false {
+//                        cell.buttonTitle = "pending"
+//                    }
+//
+//                    else if
+
+
+
+//                    if friendRequest.accepted == false && friendRequest.ownerDidSend == true {
+//                        cell.buttonTitle = "pending"
+//                    } else if friendRequest.accepted == false && friendRequest.ownerDidSend == false {
+//                        cell.buttonTitle = "accept"
+//                    }
+
+//
+//                }
+//            } else {
+//                cell.buttonTitle = "add friend"
+//            }
+//        }
+
--- GymBuddy/GymBuddy/Views/Custom Views/TableView Cells/FriendTableViewCell.swift
+++ GymBuddy/GymBuddy/Views/Custom Views/TableView Cells/FriendTableViewCell.swift
@@ -15,12 +15,50 @@ class FriendTableViewCell: UITableViewCell {
     
     //MARK: - Properties
     var buttonTitle: String?
+    var friendRequest: FriendRequest?
     
     var user: User? {
         didSet {
             updateViews()
         }
     }
+    
+    //MARK: - Actions
+    @IBAction func cellButtonTapped(_ sender: UIButton) {
+        if sender.titleLabel?.text == "accept" {
+            guard let friendRequest = friendRequest else { return }
+            FriendRequestController.shared.toggleFriendAcceptance(response: true, request: friendRequest) { result in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(_):
+                        
+                        self.updateButtons(buttonTitle: "added")
+                        print("Successfully accepted friend request")
+                    case .failure(let error):
+                        print("Error in \(#function) : On Line \(#line) : \(error.localizedDescription) \n---\n \(error)")
+                    }
+                }
+            }
+        }
+        
+        if sender.titleLabel?.text == "add friend" {
+            guard let user = user else { return }
+            FriendRequestController.shared.createFriendRequest(friendUser: user) { result in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let friendRequests):
+                        guard let friendRequest = friendRequests?.first else { return }
+                        guard let currentUser = UserController.shared.currentUser else { return }
+                        currentUser.friendRequests?.append(friendRequest)
+                        self.updateButtons(buttonTitle: "pending")
+                        print("Successfully sent friend request")
+                    case .failure(let error):
+                        print("Error in \(#function) : On Line \(#line) : \(error.localizedDescription) \n---\n \(error)")
+                    }
+                }
+            }
+        }
+    }//end of func
     
     //MARK: - Functions
     fileprivate func updateViews() {
@@ -34,6 +72,20 @@ class FriendTableViewCell: UITableViewCell {
         cellButton.setTitleColor(.white, for: .normal)
         cellButton.setTitle(buttonTitle, for: .normal)
         
+
+        
+//        if let randomInt = [0,1].shuffled().first {
+//            cellButton.setTitle(buttonTitles[randomInt], for: .normal)
+//            if randomInt == 0 {
+//                cellButton.setBackgroundColor(.customLightGreen!)
+//            } else {
+//                cellButton.setBackgroundColor(.gray)
+//            }
+//        }
+        updateButtons(buttonTitle: buttonTitle)
+    }
+    
+    fileprivate func updateButtons(buttonTitle: String) {
         if buttonTitle == "added" {
             cellButton.setBackgroundColor(.gray)
             cellButton.isEnabled = false
@@ -48,16 +100,6 @@ class FriendTableViewCell: UITableViewCell {
             cellButton.setBackgroundColor(.customLightGreen!)
             cellButton.isEnabled = true
         }
-        
-//        if let randomInt = [0,1].shuffled().first {
-//            cellButton.setTitle(buttonTitles[randomInt], for: .normal)
-//            if randomInt == 0 {
-//                cellButton.setBackgroundColor(.customLightGreen!)
-//            } else {
-//                cellButton.setBackgroundColor(.gray)
-//            }
-//        }
-        
     }
 
 }//End of class
